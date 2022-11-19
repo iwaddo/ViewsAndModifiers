9b4cf456ab46079aa6ad581449ccdf02b535c3a7
Ian Waddington <iwaddo@icloud.com>
2022-11-19 21:12:41 +0000

    D23 Before Custom Containers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var useRedText = false
+struct Watermark: ViewModifier {
+    var text: String
     
-    @ViewBuilder var spells: some View {
-        VStack {
-            Text("Lumos")
-            Text("Obliviate")
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
         }
     }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+    
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
 
 struct CapsuleText: View {
     var text: String
@@ -28,25 +58,45 @@ struct CapsuleText: View {
             .clipShape(Capsule())
     }
 }
+
+struct ContentView: View {
+    @State private var useRedText = false
+    
+    @ViewBuilder var spells: some View {
+        VStack {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
     
     var body: some View {
-        HStack{
+                   
             VStack(spacing: 10) {
+                Color.blue
+                    .frame(width: 200, height: 300)
+                    .watermarked(with: "Hacking with Swift")
+                
+                
+                Text("Cheeky Chappy")
+                    .modifier(Title())
                 Text("Gryffindor")
+                    .titleStyle()
                 Text("Hufflepuff")
                     .font(.largeTitle)
                 Text("Ravenclaw")
                 Text("Slytherin")
+                    .titleStyle()
                 
                 CapsuleText(text: "Wombat")
                 CapsuleText(text: "Weevil")
+                spells
             }
             .font(.title)
             
-            spells
+    
             
         }
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
