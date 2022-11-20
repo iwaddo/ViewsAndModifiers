a60e089603b4806ed356238932fe5a08306c6441
Ian Waddington <iwaddo@icloud.com>
2022-11-20 08:37:24 +0000

    Before Switching Branches

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,97 +7,34 @@
 
 import SwiftUI
 
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
-
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-    
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-struct CapsuleText: View {
-    var text: String
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
     
     var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .background(.blue)
-            .clipShape(Capsule())
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView: View {
-    @State private var useRedText = false
-    
-    @ViewBuilder var spells: some View {
-        VStack {
-            Text("Lumos")
-            Text("Obliviate")
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+            
         }
     }
-    
-    var body: some View {
-                   
-            VStack(spacing: 10) {
-                Color.blue
-                    .frame(width: 200, height: 300)
-                    .watermarked(with: "Hacking with Swift")
-                
-                
-                Text("Cheeky Chappy")
-                    .modifier(Title())
-                Text("Gryffindor")
-                    .titleStyle()
-                Text("Hufflepuff")
-                    .font(.largeTitle)
-                Text("Ravenclaw")
-                Text("Slytherin")
-                    .titleStyle()
-                
-                CapsuleText(text: "Wombat")
-                CapsuleText(text: "Weevil")
-                spells
-            }
-            .font(.title)
-            
-    
-            
-        }
-    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
